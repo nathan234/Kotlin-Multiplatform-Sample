5b1af64d8a833c0dd7933440a8ca5e5a08825ac0
Nathan Retta <nretta@gmail.com>
2021-03-09 21:56:32 -0700

    adding android and ios specific dependencies

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 val ktorVersion = "1.5.2"
 val coroutineVersion = "1.4.3"
 val serializationVersion = "1.1.0"
+val coroutinesKtxVersion = "1.3.2"
 
 plugins {
     kotlin("multiplatform")
@@ -38,6 +39,10 @@ kotlin {
         val androidMain by getting {
             dependencies {
                 implementation("com.google.android.material:material:1.3.0")
+                implementation("androidx.core:core-ktx:$coroutinesKtxVersion")
+                implementation(
+                    "org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutineVersion")
+                implementation("io.ktor:ktor-client-android:$ktorVersion")
             }
         }
         val androidTest by getting {
@@ -46,7 +51,11 @@ kotlin {
                 implementation("junit:junit:4.13.1")
             }
         }
-        val iosMain by getting
+        val iosMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-client-ios:$ktorVersion")
+            }
+        }
         val iosTest by getting
     }
 }
